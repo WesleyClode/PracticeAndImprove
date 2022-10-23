c7c390ff1cf4d79a65c495319c4b95a0c25184ec
cluo <13855268721@163.com>
2022-10-23 10:53:34 +0800

    回调函数

---
+++ 章节_02_手写源码系列/核心知识点练习/回调函数进阶.js
@@ -0,0 +1,63 @@
+/**
+ * 普通回调函数
+ * 把一函数当做参数传给then函数，then函数里调用这一函数并传值，这一函数能收到值
+ */
+
+function then(cb){
+  cb('回调值')
+}
+then((data)=>{
+  console.log('接收:'+data)
+})
+
+/**
+ * 带点业务的普通回调函数
+ */
+function source(){
+  let des = ''
+  then(item=>{
+    des = item
+  })
+  console.log('source得到了:',des)
+}
+function then(cb){
+  // 通过xxx逻辑得到了值100
+  cb('then的100')
+}
+source()
+/**
+ * 特殊回调函数，回调函数里的参数也是回调函数
+ */
+
+let promise = function(cb){
+  let resolve = function(value){
+    console.log('resolve:', value)
+    return 'resolve'
+  }
+  let reject = function(){
+    console.log('reject')
+    return 'reject'
+  }
+  cb(resolve,reject)
+}
+//promise里传一个函数，函数接收resolve,reject两个函数作为参数
+//promise()后，开始走promise的函数体，开始走cb(resolve,reject)，走cb的函数体
+//发现resolve('test')，调用函数resolve('test'), resolve函数体开始走，接收值
+
+/**
+ * 正常模式
+ * 回调函数flag1: function flag1(data){}
+ * 回调函数flag2: function flag2(data){}
+ * promise(flag1)，申明:function promise(cb)，  赋值：cb('test')， flag1收到data
+ * hard模式
+ * promise(flag1)，申明:function promise(cb)，  赋值：cb(flag2)， flag1收到一个flag2，赋值：flag2('test')， flag2收到data
+ * 回调函数核心思路就是如果回调函数里需要值，那就一方申明了，另一方调用传值 
+*/
+// 正常模式
+promise((resolve,reject)=>{
+  console.log(resolve,reject)
+})
+// hard模式
+promise((resolve,reject)=>{
+  resolve('test')
+})
