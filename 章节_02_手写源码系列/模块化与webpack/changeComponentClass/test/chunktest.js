b33d935e2e87821d9a1e62e050217ae1c0d74445
常凯 <kevin.chang@71360.com>
2022-12-02 18:23:40 +0800

    babel插件开发调试

---
+++ 章节_02_手写源码系列/模块化与webpack/changeComponentClass/test/chunktest.js
@@ -0,0 +1,16 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+  ["chunk-1b380b73"],
+  {
+    b5db: function () {
+      "use strict";
+      var a = function () {
+          var e = this,
+            t = e._self._c;
+          return t(
+            "div",
+            { staticClass: "el-contentArea" }
+          );
+        }
+    }
+  },
+]);
